2167517b249b9fad8359fd1dd8a4b54e2a64c204
Dmytro Melnychenko <51658169+bee7ch7@users.noreply.github.com>
2022-12-27 09:07:24 +0100

    Update outputs.tf

--- ecs/task-definition/outputs.tf
+++ ecs/task-definition/outputs.tf
@@ -6,16 +6,12 @@ output "aws_ecs_task_definition_log_group" {
   value = aws_cloudwatch_log_group.this[0].name
 }
 
-# output "policies" {
-#   value = module.policies.policy_arn
-# }
-
 output "task_policies" {
-  value = try(module.task_policies[0].list_of_policy_arn, "")
+  value = try(module.task_policies[0].list_of_policy_arns, [])
 }
 
 output "task_execution_policies" {
-  value = try(module.task_execution_policies[0].list_of_policy_arn, "")
+  value = try(module.task_execution_policies[0].list_of_policy_arns, [])
 }
 
 output "task_execution_iam_role_arn" {
