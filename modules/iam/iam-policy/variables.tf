261789f19dc6b06d81c2c4255041ce7c67185649
Dmytro Melnychenko <dmytro.melnychenko@sigma.software>
2023-01-06 09:19:58 +0100

    fix: new things 2

--- modules/iam/iam-policy/variables.tf
+++ modules/iam/iam-policy/variables.tf
@@ -28,9 +28,9 @@
 #   default     = ""
 # }
 
-# variable "tags" { ############
-#   description = "A map of tags to add to all resources." #####
-#   type        = map(string) ############
+# variable "tags" { ##############
+#   description = "A map of tags to add to all resources." #######
+#   type        = map(string) ##############
 #   default     = {} ############
 # }
 
