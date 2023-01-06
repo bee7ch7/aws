3672acb99dd0fdb00269f08fd744225eec574c2b
Dmytro Melnychenko <dmytro.melnychenko@sigma.software>
2023-01-06 09:12:41 +0100

    feat: new things

--- modules/iam/iam-policy/variables.tf
+++ modules/iam/iam-policy/variables.tf
@@ -28,8 +28,8 @@
 #   default     = ""
 # }
 
-# variable "tags" { #
-#   description = "A map of tags to add to all resources." #
+# variable "tags" { ###
+#   description = "A map of tags to add to all resources." ###
 #   type        = map(string) ###
 #   default     = {} ###
 # }
